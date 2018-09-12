4057eedc384d55e087948ec40d17237491d7acaa
hmpfeng <hmpfeng@163.com>
2018-09-12 16:31:44 +0800

    分包处理

---
+++ app/src/main/java/com/rs/ys/mvpdemo/http/ApiTag.java
@@ -0,0 +1,9 @@
+package com.rs.ys.mvpdemo.http;
+
+/**
+ * Created by 谢岳峰 on 2018/8/28.
+ */
+public interface ApiTag {
+    int BRANDS = 50001;//获取所有衣服品牌信息接口
+    int BRANDS_TEST = 50002;//获取所有衣服品牌信息接口
+}
